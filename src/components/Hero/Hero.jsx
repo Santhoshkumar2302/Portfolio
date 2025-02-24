118a301e38d9586c449d4d1bb2de9e2dcaa4402e
Santhosh Kumar G <112610326+Santhoshkumar2302@users.noreply.github.com>
2025-02-24 06:21:43 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import './Hero.css'
-import profile from '../../assets/profile.jpg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import Santhosh_Kumar_Resume from '../../assets/Santhosh_Kumar_Resume.pdf'
 
 const Hero = () => {
   return (
     <div id='home' className='hero'>
-       //<img src={profile} alt="" className='profile' />
+       <img src={profile} alt="" className='profile' />
         <h1><span>I'm Santhosh Kumar G,</span> Fullstack developer Based in Tindivanam.</h1>
         <p>I am a FullStack Developer and completed B.Tech(IT) in 2023.</p>
         <div className="hero-action">
